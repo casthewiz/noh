ecb0a2f1f6696b1704daece0e74e414c5199d8a9
Cassidy Schaufele <cschaufe@kent.edu>
2016-01-20 15:23:22 -0500

    Added ohNoh function to index.js
    ohNoh is a function which immediately executes the given function
    passed as a parameter, with arguments for said function contained
    within the second argument for ohNoh( ).

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 module.exports = {
   wearNoh: function(f, e, time){
     f = addMask(f);
-
     return function(){
       console.time("Execution Time");
       var l = f.apply(this, arguments);
@@ -18,6 +17,25 @@ module.exports = {
       }
       return l
     }
+  },
+  ohNoh: function(f, args, e, time){
+    if (typeof(args) != "object" || args.isArray == false){
+      tmp = args;
+      args = [];
+      args[0] = tmp
+    }
+    f = addMask(f);
+      console.time("Execution Time");
+      var l = f.apply(this, args);
+      console.timeEnd("Execution Time");
+      if (l === e){
+        console.log( e + ' === ' + l + ', assert' + "\x1b[32m" + ' passed' + "\x1b[0m" +  '!\n')
+        console.log(f.gnoh)
+      } else {
+        console.log(e + ' !== ' + l + ', assert' + "\x1b[31m" +' failed' + "\x1b[0m" + '.\n')
+        console.log(f.fnoh)
+      }
+      return l
   }
 }
 
